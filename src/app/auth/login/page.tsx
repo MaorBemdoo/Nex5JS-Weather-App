cceed9f79c1195ba5ed8c6932931234d61ca5670
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-08-16 11:16:14 +0100

    install react-tostify

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,4 +1,14 @@
 export default function Login() {
+
+    const login = async() => {
+        const res = fetch("/ap/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
     return (
         <form className="px-8 flex flex-col gap-4 is-[input]:*:block is-[input]:*:p-4 is-[input]:*:h-12 is-[input]:*:w-full is-[input]:*:rounded-md is-[input]:*:border is-[input]:*:border-gray-300 is-[input]:*:shadow-sm focus:is-[input]:*:border-indigo-300 focus:is-[input]:*:ring focus:is-[input]:*:ring-indigo-200 focus:is-[input]:*:ring-opacity-50">
             <input type="text" placeholder="Username"/>
